60563f021d8b5a27b475c289e8b71229cf576b87
rnfltpgus <jpp901220@gmail.com>
2022-10-05 22:20:19 +0900

    🔩 Feat: Quill 셋팅

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useCallback } from 'react';
+import Quill from 'quill';
+import 'quill/dist/quill.snow.css';
+
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  ['bold', 'italic', 'underline'],
+  [{ color: [] }, { background: [] }],
+  [{ script: 'sub' }, { script: 'super' }],
+  [{ align: [] }],
+  ['image', 'blockquote', 'code-block'],
+  ['clean'],
+];
 
 const TextEditor = () => {
-  return (
-    <div>TextEditor</div>
-  )
-}
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
 
-export default TextEditor
+    wrapper.innerHTML = '';
+    const editor = document.createElement('div');
+    wrapper.append(editor);
+    new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } });
+  }, []);
+
+  return <div className='container' ref={wrapperRef}></div>;
+};
+
+export default TextEditor;
