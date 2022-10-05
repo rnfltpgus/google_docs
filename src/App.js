e18410d324b9768b7807b853bff79ce934ed299a
rnfltpgus <jpp901220@gmail.com>
2022-10-06 02:14:45 +0900

    🔩 Feat: uuid를 적용한 docs간 고유 방 url 생성, docs 간 동시 전파 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import { v4 as uuidV4 } from 'uuid';
+
 import TextEditor from './TextEditor';
 
 const App = () => {
   return (
-    <>
-      <TextEditor />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' exact>
+          <Route
+            path='/documents'
+            element={<Navigate replace to={`/documents/${uuidV4()}`} />}
+          />
+        </Route>
+        <Route path='/documents/:id' element={<TextEditor />}></Route>
+      </Routes>
+    </Router>
   );
 };
 
--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,4 +1,6 @@
-import React, { useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+
+import { io } from 'socket.io-client';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 
@@ -15,13 +17,61 @@ const TOOLBAR_OPTIONS = [
 ];
 
 const TextEditor = () => {
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+
+  useEffect(() => {
+    const socketConnection = io('http://localhost:3001');
+    setSocket(socketConnection);
+
+    return () => {
+      socketConnection.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+
+    const handler = (delta) => {
+      quill.updateContents(delta);
+    };
+
+    socket.on('receive-changes', handler);
+
+    return () => {
+      socket.off('receive-changes', handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+
+    const handler = (delta, oldDelta, source) => {
+      if (source !== 'user') return;
+
+      socket.emit('send-changes', delta);
+    };
+
+    quill.on('text-change', handler);
+
+    return () => {
+      quill.off('text-change', handler);
+    };
+  }, [socket, quill]);
+
   const wrapperRef = useCallback((wrapper) => {
+    const editor = document.createElement('div');
+
     if (wrapper == null) return;
 
     wrapper.innerHTML = '';
-    const editor = document.createElement('div');
     wrapper.append(editor);
-    new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } });
+
+    const q = new Quill(editor, {
+      theme: 'snow',
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+    setQuill(q);
   }, []);
 
   return <div className='container' ref={wrapperRef}></div>;
